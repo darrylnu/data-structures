d8a85057964c186599a146bf20d5f0cb8b5cb15a
Darryl Nunn <darrylnu@gmail.com>
2015-10-08 16:53:11 -0700

    Completed sprint 1

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.index = 0;
+  this.storage = {};
 };
 
+var obj = new Queue();
 
+Queue.prototype = {
+  size : function(){
+    return this.index;
+  },
+
+  enqueue : function(val){
+    this.storage[this.index] = val;
+    this.index++;
+  },
+
+  dequeue : function(){
+    if(this.index > 0){
+      var dequeued = this.storage[0];
+      delete this.storage[0]
+      this.storage[0] = this.storage[1];
+      this.index--;
+      return dequeued;
+    }
+  },
+    constructor : Queue
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-};
+  this.index = 0;
+  this.storage = {};
+  };
+
+var obj = new Stack();
 
 
+Stack.prototype = {
+  size :function(){
+  return this.index;
+  },
+  push : function(val){
+    this.storage[this.index] = val;
+    this.index++;
+  },
+  pop : function(){
+    if(this.index >0) {
+    var popped = this.storage[this.index-1];
+    delete this.storage[this.index-1];
+    this.index--;
+    return popped;
+    }
+  },
+  constructor : Stack
+  };
