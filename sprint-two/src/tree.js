4376c714f378952bca7e5fe32d6d577369b9d98f
darrylnu <darrylnu@gmail.com>
2015-10-09 12:38:19 -0700

    linkedList complete, Tree worked on

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -26,10 +26,8 @@ treeMethods.contains = function(target){
 for(var i = 0; i < this.children.length; i++) {
     if(this.children[i].value === target) {
       return true;
-    } else if(i === this.children.length)
-    this.children[i].contains(target);
-      return false;
-    }  
+    }
+  return false;
 };
 
 
