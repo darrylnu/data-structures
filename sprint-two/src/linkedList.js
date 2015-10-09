651393aad1ddb9109508876af48df64b9aa67996
Darryl Nunn <darrylnu@gmail.com>
2015-10-09 11:10:12 -0700

    completed linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,21 +2,41 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  // var index = 0;
+  // console.log(Linke)
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    list.tail = newNode;
+    if(list.head === null){
+      list.head = newNode;
+    }
+// console.log(list);
   };
 
   list.removeHead = function(){
+    var oldHead = list.head['value'];
+    list.head = list.tail;
+    // console.log(oldHead);
+    return oldHead;
   };
 
   list.contains = function(target){
+    console.log(list);
+    if(list.head.value === target || list.tail.value === target){
+      return true;
+    }
+    // for(var key in list){        
+    //   if(list[key].value === target){
+    //     return true;
+    
+         return false;
   };
 
   return list;
 };
-
 var Node = function(value){
-  var node = {};
+  var node = {}; 
 
   node.value = value;
   node.next = null;
