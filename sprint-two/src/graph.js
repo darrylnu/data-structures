0f67eb52c7eaf8d0b96fe338f09bc3a5fd0e5c29
darrylnu <darrylnu@gmail.com>
2015-10-09 22:11:24 -0700

    Started on graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,21 +1,54 @@
 
 
 var Graph = function(){
+  // console.log('this is the Graph',Graph)
+  // var obj = {};
+  this.value;
+  this.childNodes = [];
 };
 
 Graph.prototype.addNode = function(node){
+  console.log(this.childNodes);
+  if(this.value){
+    node = new Graph();
+    // this.value = node;
+    this.childNodes.push(node);
+  } else {
+    this.value = node; 
+  }
+
+
 };
 
 Graph.prototype.contains = function(node){
+
+  if(this.value === node) {
+    return true;
+  }
+  return false;
+
 };
 
 Graph.prototype.removeNode = function(node){
+  if(this.value === node){
+  delete this.value; //this is probably going to break our code
+    
+  } else {
+    for(var i = 0; i < this.childNodes.length; i++){
+      if(this.childNodes[i] === node){  
+      delete this.childNodes[i];
+      }
+    }
+  }
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  // console.log(this)
+
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
