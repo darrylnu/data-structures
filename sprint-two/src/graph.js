9dc9a23703e117b6902025fc105863eb3370c028
Darryl Nunn <darrylnu@gmail.com>
2015-10-18 21:24:13 -0700

    Update graph.js
    Added work from pairing session

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,65 +1,102 @@
-
-
 var Graph = function(){
-  // console.log('this is the Graph',Graph)
-  // var obj = {};
-  this.value;
-  this.childNodes = [];
+  this.nodeList = [];
+};
+
+var A_node = function(name){
+  this.name = name;
+  this.edges = [];
 };
 
 Graph.prototype.addNode = function(node){
-  console.log(this.childNodes);
-  if(this.value){
-    node = new Graph();
-    // this.value = node;
-    this.childNodes.push(node);
-  } else {
-    this.value = node; 
-  }
-
-
+  var newNode = new A_node;
+  newNode.name = node;
+  this.nodeList.push(newNode);
 };
 
 Graph.prototype.contains = function(node){
-
-  if(this.value === node) {
-    return true;
+  for(var i = 0; i < this.nodeList.length; i++){
+    if(this.nodeList[i].name === node){
+      return true;
+    }
   }
   return false;
-
 };
 
 Graph.prototype.removeNode = function(node){
-  if(this.value === node){
-  delete this.value; //this is probably going to break our code
-    
-  } else {
-    for(var i = 0; i < this.childNodes.length; i++){
-      if(this.childNodes[i] === node){  
-      delete this.childNodes[i];
-      }
+  for(var i = 0; i < this.nodeList.length; i++){
+    if(this.nodeList[i].name === node){
+      this.nodeList.splice(i , 1); 
     }
   }
+
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
-
+  for(var i = 0; i < this.nodeList.length; i++){
+    if(this.nodeList[i].name === fromNode){
+      for(var j = 0; j < this.nodeList[i].edges.length; j++){
+        if(this.nodeList[i].edges[j] === toNode){
+          return true;
+        }
+      }
+    }
+  }
+  
+  return false;
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-  // console.log(this)
+  if(fromNode === toNode){
+    for(var i = 0; i < this.nodeList.length; i++){
+      if(this.nodeList[i].name === fromNode){
+        this.nodeList[i].edges.push(toNode);
+      }
+    }
+  } else {
+    for(var i = 0; i < this.nodeList.length; i++){
+      if(this.nodeList[i].name === fromNode){
+        this.nodeList[i].edges.push(toNode);
+      }
+    }
+    for(var i = 0; i < this.nodeList.length; i++){
+      if(this.nodeList[i].name === toNode){
+        this.nodeList[i].edges.push(fromNode);
+      }
+    }  
+  }
 
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+  for(var i = 0; i < this.nodeList.length; i++){
+    if(this.nodeList[i].name === fromNode){
+      for(var j = 0; this.nodeList[i].edges.length; j++){
+        if(this.nodeList[i].edges[j] === toNode){
+          this.nodeList[i].edges.splice(j, 1);
+        }
+      }
+    }
+  }
+  for(var i = 0; i < this.nodeList.length; i++){
+    if(this.nodeList[i].name === toNode){
+      for(var j = 0; this.nodeList[i].edges.length; j++){
+        if(this.nodeList[i].edges[j] === fromNode){
+          this.nodeList[i].edges.splice(j, 1);
+        }
+      }
+    }
+  }
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for(var i = 0; i < this.nodeList.length; i++){
+    if(this.nodeList[i] !== undefined){
+      cb(this.nodeList[i].name);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
-
